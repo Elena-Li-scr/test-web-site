b3880e6acbecc4a74affd145560f2691e5fe875f
sherzod <sherzodbek.sharobiddinov1@gmain.com>
2025-12-23 11:28:56 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/sign-up" element={<SignUp />} /> */}
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
